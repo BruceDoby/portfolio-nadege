1218bde79083fa23d56bef70319a0ef510452cdf
Bruce Doby <dobybruce@gmail.com>
2025-10-21 16:38:04 +0200

    ajout de la page d'accueil et d'un certains nombre d'image ainsi que du style et préparations de certains fichiers

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import BallsHome from './assets/Boules_accueil.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div className="presentation">
+        <div className='presentation_elements'>
+          <div className='titles'>
+            <h1 className='firstground'>Nadège Choquet</h1>
+            <h1 className='background'>Nadège Choquet</h1>
+          </div>
+          <div className='rectangle_balls'>
+            <h2>Assistante polyvalente</h2>
+            <div className='liquid-glass'></div>
+            <div className="balls">
+              <img src={BallsHome} alt="image d'accueil"></img>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </main>
     </>
   )
 }
