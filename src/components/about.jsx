6bb609a97cb3c3165c668bc059a88c8d4a023f67
Bruce Doby <dobybruce@gmail.com>
2025-11-10 14:20:57 +0100

    quelques changements effectués

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -12,16 +12,19 @@ function About() {
                 <p>
                     Hi, je suis Nadège,<br />
                     <br />
-                    Bienvenue sur mon CV où je vous présente mon parcours professionel.<br />
+                    Bienvenue sur mon CV où je vous présente l'ensemble de mon parcours professionel, en tant<br />
+                    qu'assistance depuis plus de 20 ans.<br />
                     <br />
-                    Je possède plus de 20 années d'expériences et ce, dans des secteurs variés.<br />
-                    D'assistante administrative à assistante facturation actuellement, prouvant ma<br />
-                    polyvalence et mes capacités d'adaptation.<br />
+                    Polyvalente dans divers domaines, administratif, logistique, facturation... Je me considère<br />
+                    comme quelqu'un de professionel, qui fait toujours de son mieux et sur qui on peut compter.<br />
+                    J'accorde une grande importance au respect et à l'honnêteté qui sont mes principales valeurs.<br />
                     <br />
-                    Mes atouts pour votre entreprise : on peut dire de moi que je fais preuve de rigueur,<br />
-                    de fiabilité et que je m'investis dans tout les postes qui me sont confiés.<br />
+                    Plus personnellement, je me suis découvert une passion pour le design et me suis formée seule<br />
+                    sur Figma, qui est un outil qui permets de créer des designs de site web.<br />
+                    J'aime également voyager et la photographie. Je fais régulièrement du sport, gym et course à<br />
+                    pied.<br />
                     <br />
-                    Disponible pour étudier toute proposition pertinente.
+                    N'hésitez pas à me contacter pour en savoir plus.
                 </p>
                 <div>
                     <img src={Polygon} alt='Polygon' className='polygon1'></img>
